2a1b65114025fa56e94cf73ec268ffc5790c3454
DRNXR6 <darienaguilar3000@gmail.com>
2025-04-15 15:45:06 -0600

    Quiz2 Python

--- Ejercicio1/CifradoCésar.py
+++ Ejercicio1/CifradoCésar.py
@@ -7,7 +7,7 @@ class Juego:
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
         ]
-        self.numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # NUEVA LISTA
+        self.numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.nivel_Cifrado = 0
         self.lista_frase_cifrada = []
         self.lista_frase_decifrada = []
